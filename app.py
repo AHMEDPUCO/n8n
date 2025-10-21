eda6dc1cbd936c8242937af1599b79514d1d3992
Ahmed Puco <ahmedpuco03@gmail.com>
2025-10-21 13:11:09 +0000

    Add new file

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+import os
+import subprocess
+
+def vulnerable_exec(command):
+    # ¡Vulnerabilidad! Semgrep debería detectar esto.
+    os.system(command)
+
+def another_vulnerable_call(request):
+    # ¡Vulnerabilidad! Inyección de comandos.
+    cmd = request.args.get('cmd')
+    subprocess.run(cmd, shell=True)
+
+print("Archivo vulnerable de prueba")
