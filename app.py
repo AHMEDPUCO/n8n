320a4acc0e462ee0fae384464019df5d030ff7a3
Ahmed Puco <ahmedpuco03@gmail.com>
2026-01-19 14:07:49 +0000

    Edit app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/run")
 def run():
     cmd = request.args.get("cmd")
-    os.system(cmd)  # ❌ Semrep: commandon_pruebas
+    os.system(cmd)  # ❌ Semrep: comandon_pruebas
     return "okay"
 
 # 2️⃣ Command Injection (subprocess + shell=True)
